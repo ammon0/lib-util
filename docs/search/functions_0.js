69bd82dcf822f9ab4e0ad09580932275cfb053ca
Ammon Dodson <ammon@ammondodson.info>
2016-11-25 18:10:32 -0600

    regenerate documentation

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -7,9 +7,9 @@ var searchData=
   ['ds_5ffind',['DS_find',['../data_8h.html#ac33b13e61fd47f989f61940fbb50d706',1,'data.h']]],
   ['ds_5ffirst',['DS_first',['../data_8h.html#a6cc7722834e766ed57895f1d1460ac60',1,'data.h']]],
   ['ds_5fflush',['DS_flush',['../data_8h.html#a05b9db783e9d4b021355fa6a9856c75b',1,'data.h']]],
-  ['ds_5finsert',['DS_insert',['../data_8h.html#a76bbc7e7efc4e048596fca5b59fca342',1,'data.h']]],
-  ['ds_5finsert_5ffirst',['DS_insert_first',['../data_8h.html#ad0a7806c31fdf410a72664dea0c9ebba',1,'data.h']]],
-  ['ds_5finsert_5flast',['DS_insert_last',['../data_8h.html#a565539fe84b95b2e749da52ac9a4f301',1,'data.h']]],
+  ['ds_5finsert',['DS_insert',['../data_8h.html#a5390b657446b15f8397eb9705af3ca4a',1,'data.h']]],
+  ['ds_5finsert_5ffirst',['DS_insert_first',['../data_8h.html#aa3f00b82568d5c0bb7b43d79c3b35c62',1,'data.h']]],
+  ['ds_5finsert_5flast',['DS_insert_last',['../data_8h.html#a75725b5a9e0b4a7786cdec269015fbdd',1,'data.h']]],
   ['ds_5fisempty',['DS_isempty',['../data_8h.html#a37b448842fea9b7997425d0b23d9f405',1,'data.h']]],
   ['ds_5fisleaf',['DS_isleaf',['../data_8h.html#a60a14d170726a82c38b1ccc24044c851',1,'data.h']]],
   ['ds_5flast',['DS_last',['../data_8h.html#a62d6c18761a826c59622d25a4e435854',1,'data.h']]],
@@ -19,6 +19,5 @@ var searchData=
   ['ds_5fprevious',['DS_previous',['../data_8h.html#a2d268d31c093b31b9afd3750df701125',1,'data.h']]],
   ['ds_5fremove',['DS_remove',['../data_8h.html#a877da7a87a9b87a69e2153a97abab91a',1,'data.h']]],
   ['ds_5fremove_5ffirst',['DS_remove_first',['../data_8h.html#a8d5e4bcac50816790fddd053549e5d17',1,'data.h']]],
-  ['ds_5fremove_5flast',['DS_remove_last',['../data_8h.html#ab06d25e611009bd3469bd2cef9999583',1,'data.h']]],
-  ['ds_5fsort',['DS_sort',['../data_8h.html#a1a131827091bdacd3b084a9a9c840693',1,'data.h']]]
+  ['ds_5fremove_5flast',['DS_remove_last',['../data_8h.html#ab06d25e611009bd3469bd2cef9999583',1,'data.h']]]
 ];
